42dbc07a21494b1dd267f4eb7be4edb3666a06e1
createkuntal-ship-it <create.kuntal@gmail.com>
2026-02-17 16:29:05 +0530

    Update preload.js

--- src/preload.js
+++ src/preload.js
@@ -1,13 +1,15 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('api', {
-  getSettings:    ()         => ipcRenderer.invoke('get-settings'),
-  saveSettings:   (s)        => ipcRenderer.invoke('save-settings', s),
-  getCollection:  ()         => ipcRenderer.invoke('get-collection'),
-  saveCollection: (c)        => ipcRenderer.invoke('save-collection', c),
-  openUrl:        (url)      => ipcRenderer.invoke('open-url', url),
-  groq:           (opts)     => ipcRenderer.invoke('groq-request', opts),
-  pexels:         (opts)     => ipcRenderer.invoke('pexels-search', opts),
-  pixabay:        (opts)     => ipcRenderer.invoke('pixabay-search', opts),
+  getSettings:    ()              => ipcRenderer.invoke('get-settings'),
+  saveSettings:   (s)             => ipcRenderer.invoke('save-settings', s),
+  getCollection:  ()              => ipcRenderer.invoke('get-collection'),
+  saveCollection: (c)             => ipcRenderer.invoke('save-collection', c),
+  openUrl:        (url)           => ipcRenderer.invoke('open-url', url),
+  groq:           (opts)          => ipcRenderer.invoke('groq-request', opts),
+  pexels:         (opts)          => ipcRenderer.invoke('pexels-search', opts),
+  pixabay:        (opts)          => ipcRenderer.invoke('pixabay-search', opts),
+  readFile:       (filePath)      => ipcRenderer.invoke('read-file', filePath),
+  downloadVideo:  (url, filename) => ipcRenderer.invoke('download-video', { url, filename }),
   platform: process.platform
 });
